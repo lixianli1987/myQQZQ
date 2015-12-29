e268c885fe8dd2ea5a0846fe4384a6081a006364
@lixianli1987 <646038584@qq.com>
2015-12-29 10:31:59 +0800

    me

--- QQZQ/Source/ViewControllers/Me/MeViewController.h
+++ QQZQ/Source/ViewControllers/Me/MeViewController.h
@@ -10,4 +10,31 @@
 
 @interface MeViewController : UIViewController
 
+//个人资料设置
+@property (weak,nonatomic) IBOutlet UIButton *buttonSetPersonalData;
+//个人资料设置view
+@property (weak,nonatomic) IBOutlet UIView *viewSetPersonalData;
+//个人头像
+@property (weak,nonatomic) IBOutlet UIImageView *imageViewHeaderImg;
+//昵称
+@property (weak,nonatomic) IBOutlet UILabel *lableNickName;
+//圈圈号
+@property (weak,nonatomic) IBOutlet UILabel *labelQuanQuanNum;
+
+//邀请朋友
+@property (weak,nonatomic) IBOutlet UIView *viewInviteFriend;
+//我的圈圈钱包
+@property (weak,nonatomic) IBOutlet UIView *viewMyWallet;
+//我的相册
+@property (weak,nonatomic) IBOutlet UIView *viewMyPhoto;
+//我的历史活动
+@property (weak,nonatomic) IBOutlet UIView *viewMyActivity;
+//消息
+@property (weak,nonatomic) IBOutlet UIView *viewMessage;
+//设置
+@property (weak,nonatomic) IBOutlet UIView *viewMySet;
+
+//响应按钮方法
+-(IBAction)clickBtn:(id)sender;
+
 @end
--- QQZQ/Source/ViewControllers/Me/MeViewController.m
+++ QQZQ/Source/ViewControllers/Me/MeViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "MeViewController.h"
+#import "PersonalCardViewController.h"
+#import "MyQuanQuanWalletViewController.h"
+#import "MyHistoryActivityViewController.h"
+#import "SetViewController.h"
+
 
 @interface MeViewController ()
 
@@ -14,24 +19,136 @@
 
 @implementation MeViewController
 
+@synthesize viewSetPersonalData;
+@synthesize imageViewHeaderImg;
+@synthesize lableNickName;
+@synthesize labelQuanQuanNum;
+@synthesize viewInviteFriend;
+@synthesize viewMyWallet;
+@synthesize viewMyPhoto;
+@synthesize viewMyActivity;
+@synthesize viewMessage;
+@synthesize viewMySet;
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    //设置个人资料
+    UITapGestureRecognizer *sinTapviewSetPersonalData= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewSetPersonalData addGestureRecognizer:sinTapviewSetPersonalData];
+    //邀请朋友
+    UITapGestureRecognizer *sinTapviewInviteFriend= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewInviteFriend addGestureRecognizer:sinTapviewInviteFriend];
+    //我的圈圈钱包
+    UITapGestureRecognizer *sinTapviewMyWallet= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewMyWallet addGestureRecognizer:sinTapviewMyWallet];
+    //我的相册
+    UITapGestureRecognizer *sinTapviewMyPhoto= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewMyPhoto addGestureRecognizer:sinTapviewMyPhoto];
+    //我的历史活动
+    UITapGestureRecognizer *sinTapviewMyActivity= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewMyActivity addGestureRecognizer:sinTapviewMyActivity];
+    //消息
+    UITapGestureRecognizer *sinTapviewMessage= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewMessage addGestureRecognizer:sinTapviewMessage];
+    //设置
+    UITapGestureRecognizer *sinTapviewMySet= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sinTapView:)];
+    [viewMySet addGestureRecognizer:sinTapviewMySet];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//单击手势
+-(void)sinTapView:(UITapGestureRecognizer *)sender
+{
+    int viewTag =sender.view.tag;
+    NSLog(@"点击了%d",viewTag);
+    switch (viewTag) {
+        case 10:
+            NSLog(@"点击了设置个人资料");
+            break;
+        case 11:
+            NSLog(@"点击了邀请朋友");
+            break;
+        case 12:
+            NSLog(@"点击了我的圈圈钱包");
+            [self jumpToviewMyWallet];
+            break;
+        case 13:
+            NSLog(@"点击了我的相册");
+            break;
+        case 14:
+            NSLog(@"点击了我的历史活动");
+            [self jumpToviewMyActivity];
+            break;
+        case 15:
+            NSLog(@"点击了消息");
+            break;
+        case 16:
+            NSLog(@"点击了设置");
+            [self jumpToviewMySet];
+            break;
+        default:
+            break;
+    }
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//跳往设置个人资料页面
+-(void) jumpToviewSetPersonalData
+{
+    NSLog(@"个人资料页面");
 }
-*/
+//跳往设置个人资料页面
+-(void) jumpToviewInviteFriend
+{
+    NSLog(@"邀请朋友页面");
+}
+//跳往设置个人资料页面
+-(void) jumpToviewMyWallet
+{
+    NSLog(@"我的圈圈钱包页面");
+    MyQuanQuanWalletViewController *myQuanQuanWalletViewController = [[MyQuanQuanWalletViewController alloc] init];
+    [self.navigationController pushViewController:myQuanQuanWalletViewController animated:YES];
+}
+//跳往设置个人资料页面
+-(void) jumpToviewMyPhoto
+{
+    NSLog(@"我的相册页面");
+}
+//跳往我的历史活动
+-(void) jumpToviewMyActivity
+{
+    NSLog(@"我的历史活动页面");
+    MyHistoryActivityViewController *myHistoryActivityViewController = [[MyHistoryActivityViewController alloc] init];
+    [self.navigationController pushViewController:myHistoryActivityViewController animated:YES];
+}
+//跳往设置个人资料页面
+-(void) jumpToviewMessage
+{
+    NSLog(@"消息页面");
+}
+//跳往设置个人资料页面
+-(void) jumpToviewMySet
+{
+    NSLog(@"设置页面");
+    SetViewController *setViewController = [[SetViewController alloc] init];
+    [self.navigationController pushViewController:setViewController animated:YES];
+}
+
+
+//按钮执行方法
+-(IBAction)clickBtn:(id)sender
+{
+    
+    PersonalCardViewController *personalCardViewController = [[PersonalCardViewController alloc] init];
+    [self.navigationController pushViewController:personalCardViewController animated:YES];
+    
+    //返回上一级页面
+    //[self.navigationController popViewControllerAnimated:YES];
+    
+}
 
 @end
